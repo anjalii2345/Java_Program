d8bbc58292236e1e0e24ee054e75e8f12ab08006
anjalii2345 <anjalii.singh1818@gmail.com>
2025-05-20 15:49:51 +0530

    My first program

---
+++ Anjali_Project/src/anjali_project/First_Program.java
@@ -0,0 +1,12 @@
+package anjali_project;
+
+public class First_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method st
+		System.out.print("This my first program in java");
+		System.out.print("GAOOD WORK");
+		
+	}
+
+}
