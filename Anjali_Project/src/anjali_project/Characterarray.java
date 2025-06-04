48ced68331ca5544bf9bb6bba32300dde6dda167
anjalii2345 <anjalii.singh1818@gmail.com>
2025-06-04 10:40:17 +0000

    CharacterArray

---
+++ Anjali_Project/src/anjali_project/Characterarray.java
@@ -0,0 +1,9 @@
+package anjali_project;
+public class Characterarray{
+    public static void main(String args []){
+         String str1 = "Java programming";
+         char[] charArray = str1.toCharArray();
+         System.out.println("String to char array: " + str1.charAt(0));
+         System.out.println("Total number of characters: " + str1.length());
+    }
+}
