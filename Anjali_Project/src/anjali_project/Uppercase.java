2f166ad04f4ace6e89e83e8f3bd162f2df54f01d
anjalii2345 <anjalii.singh1818@gmail.com>
2025-06-18 13:31:16 +0000

    Uppercase

---
+++ Anjali_Project/src/anjali_project/Uppercase.java
@@ -0,0 +1,10 @@
+package anjali_project;
+public class Uppercase{
+    public static  void main(String[] args){
+        String a = "Java p";
+        String b = "program code";
+        System.out.println(a.toUpperCase());
+        System.out.println(b.toLowerCase());
+        
+    }
+}
